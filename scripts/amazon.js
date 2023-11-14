cecf74aa36381b8fc7a41047684fc14de36a99b0
heartbreathing <li.wang@hyperisland.se>
2023-11-14 11:08:59 +0100

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -68,7 +68,7 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         matchingItem = item;
       }
     });
-   
+
     if (matchingItem) {
       matchingItem.quantity += 1;
     } else {
@@ -76,8 +76,17 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         productId: productId,
         quantity: 1
       });
-    }  
-    console.log(cart)
+    }
+
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+
   });
 })
 
