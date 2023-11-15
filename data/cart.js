912a62181afa4caabadbd83c55ae3cd456bea467
heartbreathing <li.wang@hyperisland.se>
2023-11-15 11:50:39 +0100

    Delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,13 @@
-export const cart = [{
+export let cart = [{
   productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity:2
+  quantity: 2
 },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1
+{
+  productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+  quantity: 1
 }];
 
- export function addToCart (productId) {
+export function addToCart (productId) {
   let matchingItem;
 
   cart.forEach((cartItem) => {
@@ -24,4 +24,23 @@ export const cart = [{
       quantity: 1
     });
   }
+}
+
+export function deleteFromCart (productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId === productId) {
+      const newQuantity = cartItem.quantity - 1;
+      if (newQuantity > 0) {
+        newCart.push({
+          productId: cartItem.productId,
+          quantity: newQuantity,
+        });
+      }
+    } else {
+      newCart.push(cartItem);
+    }
+  });
+  cart = newCart;
 }
